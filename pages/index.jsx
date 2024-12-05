eb872bdac52681372076aeb604e374023b9c260d
hei <justinckhcheung@hotmail.com>
2024-12-05 17:26:56 +0800

    solitair

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,64 +1,200 @@
-'use client'
+"use client";
 
 // import { Preview } from 'src-app/Preview/Preview'
 
 export default function Page() {
-    return (
-        <>
-            <h1>Links</h1>
+  return (
+    <>
+      <h1>Links</h1>
 
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/basic/example5.html'}>Example 5</a>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/basic/example7.html'}>Example 7</a>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/basic/example8.html'}>Example 8</a>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/basic/example8b.html'}>Example 8b</a>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/basic/example8c.html'}>Example 8c</a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/basic/example5.html"}
+      >
+        Example 5
+      </a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/basic/example7.html"}
+      >
+        Example 7
+      </a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/basic/example8.html"}
+      >
+        Example 8
+      </a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/basic/example8b.html"}
+      >
+        Example 8b
+      </a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/basic/example8c.html"}
+      >
+        Example 8c
+      </a>
 
-            <div>-------------- * --------------</div>
-            <div>FullSDK Work in Progress</div>
-            <div>-------------- * --------------</div>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/wip-sdk-fullversion/SDK/agape/example8.html'}>sdk - Example 8</a>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/wip-sdk-fullversion/SDK/agape/example8b.html'}>sdk - Example 8b</a>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/wip-sdk-fullversion/SDK/agape/example8c.html'}>sdk - Example 8c</a>
+      <div>-------------- * --------------</div>
+      <div>FullSDK Work in Progress</div>
+      <div>-------------- * --------------</div>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/wip-sdk-fullversion/SDK/agape/example8.html"}
+      >
+        sdk - Example 8
+      </a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/wip-sdk-fullversion/SDK/agape/example8b.html"}
+      >
+        sdk - Example 8b
+      </a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/wip-sdk-fullversion/SDK/agape/example8c.html"}
+      >
+        sdk - Example 8c
+      </a>
 
-            <div>-------------- * --------------</div>
-            <div>Firmstudio Version (Partial SDK)</div>
-            <div>-------------- * --------------</div>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/firmstudio-sdk-debug/SDK/agape/example8.html'}>sdk - Example 8</a>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/firmstudio-sdk-debug/SDK/agape/example8b.html'}>sdk - Example 8b</a>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/firmstudio-sdk-debug/SDK/agape/example8c.html'}>sdk - Example 8c</a>
+      <div>-------------- * --------------</div>
+      <div>Firmstudio Version (Partial SDK)</div>
+      <div>-------------- * --------------</div>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/firmstudio-sdk-debug/SDK/agape/example8.html"}
+      >
+        sdk - Example 8
+      </a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/firmstudio-sdk-debug/SDK/agape/example8b.html"}
+      >
+        sdk - Example 8b
+      </a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/firmstudio-sdk-debug/SDK/agape/example8c.html"}
+      >
+        sdk - Example 8c
+      </a>
 
-            <div>-------------- * --------------</div>
-            <div>Fancy Cut Version 2024-07 (Full sdk)</div>
-            <div>-------------- * --------------</div>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/2024-07-fancy-cut-fullsdk/SDK/agape/example8.html'}>sdk - Example 8</a>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/2024-07-fancy-cut-fullsdk/SDK/agape/example8b.html'}>sdk - Example 8b</a>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/2024-07-fancy-cut-fullsdk/SDK/agape/example8c.html'}>sdk - Example 8c</a>
+      <div>-------------- * --------------</div>
+      <div>Fancy Cut Version 2024-07 (Full sdk)</div>
+      <div>-------------- * --------------</div>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/2024-07-fancy-cut-fullsdk/SDK/agape/example8.html"}
+      >
+        sdk - Example 8
+      </a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/2024-07-fancy-cut-fullsdk/SDK/agape/example8b.html"}
+      >
+        sdk - Example 8b
+      </a>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/2024-07-fancy-cut-fullsdk/SDK/agape/example8c.html"}
+      >
+        sdk - Example 8c
+      </a>
 
-            <div>-------------- * --------------</div>
-            <div>Fancy Cut Version 2024-07 (Full sdk Gen2)</div>
-            <div>-------------- * --------------</div>
-            <a className='m-3 p-3 bg-red-200 inline-block' href={'/sdk-full-gen2/SDK/agape/example8.html'}>sdk 8a - all</a>
-            <a className='m-3 p-3 bg-red-200 inline-block' href={'/sdk-full-gen2/SDK/agape/example8.html?filter=fancycut'}>fancy cut only</a>
-            <a className='m-3 p-3 bg-red-200 inline-block' href={'/sdk-full-gen2/SDK/agape/example8.html?filter=round'}>round only</a>
-            <a className='m-3 p-3 bg-red-200 inline-block' href={'/sdk-full-gen2/SDK/agape/example8b.html'}>sdk 8a - Example 8b</a>
-            <a className='m-3 p-3 bg-red-200 inline-block' href={'/sdk-full-gen2/SDK/agape/example8c.html'}>sdk 8a - Example 8c</a>
+      <div>-------------- * --------------</div>
+      <div>Fancy Cut Version 2024-07 (Full sdk Gen2)</div>
+      <div>-------------- * --------------</div>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2/SDK/agape/example8.html"}
+      >
+        sdk 8a - all
+      </a>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2/SDK/agape/example8.html?filter=fancycut"}
+      >
+        fancy cut only
+      </a>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2/SDK/agape/example8.html?filter=round"}
+      >
+        round only
+      </a>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2/SDK/agape/example8b.html"}
+      >
+        sdk 8a - Example 8b
+      </a>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2/SDK/agape/example8c.html"}
+      >
+        sdk 8a - Example 8c
+      </a>
 
-            <div>-------------- * --------------</div>
-            <div>Fancy Cut Version 2024-07 (Full sdk Gen2 - internal)</div>
-            <div>-------------- * --------------</div>
-            <a className='m-3 p-3 bg-red-200 inline-block' href={'/sdk-full-gen2-internal/SDK/agape/example8.html'}>sdk 8a - all</a>
-            <a className='m-3 p-3 bg-red-200 inline-block' href={'/sdk-full-gen2-internal/SDK/agape/example8.html?filter=fancycut'}>fancy cut only</a>
-            <a className='m-3 p-3 bg-red-200 inline-block' href={'/sdk-full-gen2-internal/SDK/agape/example8.html?filter=round'}>round only</a>
-            <a className='m-3 p-3 bg-red-200 inline-block' href={'/sdk-full-gen2-internal/SDK/agape/example8b.html'}>sdk 8a - Example 8b</a>
-            <a className='m-3 p-3 bg-red-200 inline-block' href={'/sdk-full-gen2-internal/SDK/agape/example8c.html'}>sdk 8a - Example 8c</a>
+      <div>-------------- * --------------</div>
+      <div>Fancy Cut Version 2024-07 (Full sdk Gen2 - internal)</div>
+      <div>-------------- * --------------</div>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2-internal/SDK/agape/example8.html"}
+      >
+        sdk 8a - all
+      </a>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2-internal/SDK/agape/example8.html?filter=fancycut"}
+      >
+        fancy cut only
+      </a>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2-internal/SDK/agape/example8.html?filter=round"}
+      >
+        round only
+      </a>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2-internal/SDK/agape/example8b.html"}
+      >
+        sdk 8a - Example 8b
+      </a>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2-internal/SDK/agape/example8c.html"}
+      >
+        sdk 8a - Example 8c
+      </a>
 
-            <div>-------------- * --------------</div>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/basic/example10a.html'}>Example 10a</a>
-            <div>-------------- * --------------</div>
-            <a className='m-3 p-3 bg-gray-200 inline-block' href={'/basic/example8-tester.html'}>Example 8Tester</a>
-            {/* <a className='m-3 p-3 bg-gray-200 inline-block' href={'/basic/example9-lab.html'}>example9-lab</a> */}
-            {/* <Preview></Preview> */}
-        </>
-    )
+      <div>-------------- * --------------</div>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/basic/example10a.html"}
+      >
+        Example 10a
+      </a>
+      <div>-------------- * --------------</div>
+      <a
+        className="m-3 p-3 bg-gray-200 inline-block"
+        href={"/basic/example8-tester.html"}
+      >
+        Example 8Tester
+      </a>
+      <div>-------------- * --------------</div>
+      <a
+        className="m-3 p-3 bg-red-200 inline-block"
+        href={"/sdk-full-gen2/SDK/agape/example8.html?filter=solitair"}
+      >
+        Solitaire DIY
+      </a>
+      {/* <a className='m-3 p-3 bg-gray-200 inline-block' href={'/basic/example9-lab.html'}>example9-lab</a> */}
+      {/* <Preview></Preview> */}
+    </>
+  );
 }
-
